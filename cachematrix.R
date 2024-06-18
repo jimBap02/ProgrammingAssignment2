dd2c396ad25bab7de687b717586bb42f2f31a8d3
jimBap02 <jra8192@outlook.com>
2024-06-18 14:48:35 -0400

    function def with comments for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of named function closures which 
+## represent a matrix that can cache its inverse:
+##    Name      Function    Action
+##    'set'     set(y)      sets the represented matrix to 'y'
+##    'get'     get()       returns the represented matrix
+##    'setinv'  setinv(inv) sets the inverse of the represented 
+##                          matrix to 'inv'
+##    'getinv'  getinv()    returns the inverse of the represented 
+##                          matrix, computing (and locally saving it)
+##                          as needed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ##  'x' is a matrix which is ASSUMED to be invertible. 
+    ##  NOTE: there is no check to insure that x is invertible, and 
+    ##  there is no error recovery from a non-invertible x other than
+    ##  the failure of solve() in cacheSolve
+    
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) inv <<- inv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
